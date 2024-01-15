f6b068f8f9782e61800427751075f2f00912e416
Aayush Pagare <aayushpagare21@gmail.com>
2024-01-15 15:55:41 +0530

    fix the prettier

--- config/configureApp.js
+++ config/configureApp.js
@@ -2,13 +2,12 @@
     Configure express application for each server, by passing configurations
 */
 const setUpExpress = ({ port, environment, logger }) => {
-    const express = require('express')
-    const app = express()
-      
-    app.listen(port || 3001, () => {
-      logger.info(`Node ${environment} at port ${port}`)
-    })
-  }
-  
-  module.exports = setUpExpress
-  
+  const express = require('express')
+  const app = express()
+
+  app.listen(port || 3001, () => {
+    logger.info(`Node ${environment} at port ${port}`)
+  })
+}
+
+module.exports = setUpExpress
