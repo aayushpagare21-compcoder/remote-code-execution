98030ca03339fde5d85541e4afce900de877f403
Aayush Pagare <aayushpagare21@gmail.com>
2024-01-15 12:50:32 +0530

    Added clusters code

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const { PORT, NODE_ENV } = require('./config/dotenv')
-const logger = require('./config/winston/index.js')
-const app = express()
-
-app.listen(PORT || 3001, () => {
-  logger.info(`Node ${NODE_ENV} at port ${PORT}`)
-})
