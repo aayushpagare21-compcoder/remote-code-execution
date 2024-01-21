faed70c0676eca92f1ed3e89192e14d5dcb1183a
Aayush Pagare <aayushpagare21@gmail.com>
2024-01-21 13:54:36 +0530

    added swagger

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const express = require('express')
+const swaggerUI = require('swagger-ui-express')
+const swaggerSpec = require('./swagger')
+
 const { PORT, NODE_ENV } = require('./config/dotenv')
 const logger = require('./config/winston')
 const bootstrap = require('./utils/bootstrap')
-const app = express()
-
-//bootstrap the application - esablish all connections of db, cache-db, queues
-bootstrap()
-
-// Middleware for handling errors
 const errorHandler = require('./middlewares/error.middleware')
-
-// Importing core routes for version 1 of the API
 const coreRoutes = require('./routes/v1/core.routes')
 
+//Create express app
+const app = express()
+//bootstrap the application - establish all connections of db, cache-db, queues
+bootstrap()
 // Set up routes for version 1 of the API under the '/api/v1' path
 app.use('/api/v1', coreRoutes)
-
+//Swagger: View api-docs
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 // Use the errorHandler middleware to handle errors in the application
 app.use(errorHandler)
-
 // Start the Express server and listen on the specified port (or default to 3001)
 app.listen(PORT || 3001, () => {
   logger.info(`Node ${NODE_ENV} server running at port ${PORT}`)
--- routes/v1/core.routes.js
+++ routes/v1/core.routes.js
@@ -4,17 +4,8 @@ const ApiError = require('../../utils/ApiError')
 // Create an Express router
 const router = express.Router()
 
-/**
- * Route to execute code.
- *
- * @route GET /execute-code
- * @returns {void}
- * @throws {ApiError} 500 - Internal Server Error if code execution fails.
- */
-router.get('/execute-code', async (req, res, next) => {
+router.post('/execute-code', async (req, res, next) => {
   try {
-    // TODO: Add logic to execute code
-
     // Example: throw an ApiError for internal server error
     throw new ApiError(500, 'Internal Server Error')
   } catch (error) {
@@ -25,3 +16,135 @@ router.get('/execute-code', async (req, res, next) => {
 
 // Export the router for use in other parts of the application
 module.exports = router
+
+/**
+ * @swagger
+ * /api/v1/execute-code:
+ *   post:
+ *     summary: Executes client's code on the server.
+ *     description: Takes source code and language and returns the result (output which is displayed in the terminal).
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               code:
+ *                 type: string
+ *                 required: true
+ *                 description: The source code to run.
+ *                 example: console.log('Hello World')
+ *               language:
+ *                 type: string
+ *                 required: true
+ *                 description: Programming language in which source code is written.
+ *                 enum:
+ *                   - JAVASCRIPT
+ *                   - PYTHON
+ *                   - JAVA
+ *                   - CPP
+ *     responses:
+ *       200:
+ *         description: Code successfully queued for submission.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 ack:
+ *                   type: string
+ *                   description: The Acknowledgement message.
+ *                   example: Your code is queued, results will be sent to you in few seconds.
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Bad request error message.
+ *                   enum:
+ *                     - Programming Language Not Supported.
+ *                     - Time Limit Exceeded.
+ *                   example: Programming Language Not Supported.
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Internal server error message.
+ *                   example: Server Facing Down Time.
+ */
+
+/**
+ * @swagger
+ * /api/v1/execute-code:
+ *   post:
+ *     summary: Executes client's code on the server.
+ *     description: Takes source code and language and returns the result (output which is displayed in the terminal).
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               code:
+ *                 type: string
+ *                 required: true
+ *                 description: The source code to run.
+ *                 example: console.log('Hello World')
+ *               language:
+ *                 type: string
+ *                 required: true
+ *                 description: Programming language in which source code is written.
+ *                 enum:
+ *                   - JAVASCRIPT
+ *                   - PYTHON
+ *                   - JAVA
+ *                   - CPP
+ *     responses:
+ *       200:
+ *         description: Code successfully queued for submission.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 ack:
+ *                   type: string
+ *                   description: The Acknowledgement message.
+ *                   example: Your code is queued, results will be sent to you in few seconds.
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Bad request error message.
+ *                   enum:
+ *                     - Programming Language Not Supported.
+ *                     - Time Limit Exceeded.
+ *                   example: Programming Language Not Supported.
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Internal server error message.
+ *                   example: Server Facing Down Time.
+ */
